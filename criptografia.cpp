2067ea833445ea41cc1a41e3c9b33f47cfeaefa7
Bianca Pacífico <111406262+biapacifico@users.noreply.github.com>
2024-04-05 18:34:43 -0300

    laeds2

--- criptografia.cpp
+++ criptografia.cpp
@@ -1,44 +1,67 @@
 #include <iostream>
 #include <string>
-
+#include <cctype>
+ 
 using namespace std;
 
-bool isLetra(char letra){
-    if(letra>='a' && letra<='z' || letra>='A' && letra<='Z'){
-        return true;
-    } else {
-        return false;
-    }
-}
+char cifraDeCesar(char letra){
+    string letras = "abcdefghijklmnopqrstuvwxyz";
 
-string criptografia(string frase){
-    for(int i=0; i<frase.size(); i++){
-        if(isLetra(frase[i])){
-            frase[i] = frase[i]+3;
+    bool letraMaiuscula = isupper(letra);
+    if(letraMaiuscula){
+        letra = tolower(letra);
+    }
+
+    char letraDecifrada;
+    for(int i=0; i<letras.length(); i++){
+        if(letra == letras[i]){
+            int posicao = i-5;
+            posicao < 0 ? posicao = letras.length()+posicao : posicao;
+            letraDecifrada = letras[posicao];
         }
     }
 
-    string fraseNova;
-    for(int i=(frase.size()-1); i>=0; i--){
-        fraseNova.push_back(frase[i]);
-    }
-
-    for(int i=(fraseNova.size()/2); i<fraseNova.size(); i++){
-        fraseNova[i] = fraseNova[i]-1;
-    }
-
-    return fraseNova;
+    return letraMaiuscula ? letraDecifrada = toupper(letraDecifrada) : letraDecifrada;
 }
 
-int main(){
+int main() {
+ 
+    string mensagemCodificada;
+    getline(cin, mensagemCodificada);
 
-    int casosDeTeste;
-    cin >> casosDeTeste;
-    cin.ignore();
+    string mensagemDecodificada = mensagemCodificada;
 
-    for(int i=1; i<=casosDeTeste; i++){
-        string frase;
-        getline(cin, frase);
-        cout << criptografia(frase) << "\n";
+    //substituição das letras pela cifra de césar e da # pelo espaço
+    for(int i=0; i<mensagemDecodificada.length(); i++){
+        if(isalpha(mensagemCodificada[i])){
+            mensagemDecodificada[i] = cifraDeCesar(mensagemDecodificada[i]);
+        } else {
+            mensagemDecodificada[i] = ' ';
+        }
     }
+
+    //reflexão de cada bloco de 2 letras
+    char aux0;
+    for(int i=0; i<mensagemDecodificada.length(); i+=2){
+        aux0 = mensagemDecodificada[i];
+        mensagemDecodificada[i] = mensagemDecodificada[i+1];
+        mensagemDecodificada[i+1] = aux0;
+    }
+
+    //permutação dos blocos de 2 letras
+    char aux, aux2;
+    for(int i=0, j=(mensagemDecodificada.length()-1); i<(mensagemDecodificada.length()/2), j>(mensagemDecodificada.length()/2); i+=4, j-=4){
+        aux = mensagemDecodificada[i];
+        mensagemDecodificada[i] = mensagemDecodificada[j-1];
+        mensagemDecodificada[j-1] = aux;
+
+        aux2 = mensagemDecodificada[i+1];
+        mensagemDecodificada[i+1] = mensagemDecodificada[j];
+        mensagemDecodificada[j] = aux2;
+    }
+
+    cout << "Mensagem codificada:\n" << mensagemCodificada << "\n";
+    cout << "Mensagem decodificada:\n" << mensagemDecodificada << "\n";
+ 
+    return 0;
 }
